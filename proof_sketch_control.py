ac401b6290a2158a25baffc2f8c8c18b2da5428b
MoseslGit <mliew@connect.hku.hk>
2023-12-13 14:51:43 +0000

    Add Autoformalizer GPT model and modify assistant control

--- proof_sketch_control.py
+++ proof_sketch_control.py
@@ -1,50 +1,63 @@
 import time
-
-class Proof_sketch_controller:
-
-    '''This class acts as a controller for proof sketch GPTs.
-    Each initialization should start an individual thread, which we will append messages to.'''
+class Controller:
     def __init__(self, client, assistant_id):
         self.assistant_id = assistant_id
         self.client = client
         self.my_assistant = self.client.beta.assistants.retrieve(assistant_id)
         self.thread = self.client.beta.threads.create()
 
-    def add_context(self, problem_description):
-        """
-        Enhances the GPT query with additional context from the reasoning framework.
-        """
-        # Implement logic to add context to the problem description
-        enhanced_query = problem_description  # Placeholder
-        return enhanced_query
-
     def query_gpt(self, query):
         """
         Sends the formatted query to the GPT model and returns the response.
         """
-        message = self.client.beta.threads.messages.create(
-        thread_id=self.thread.id,
-        role="user",
-        content=f"{query}"
-        )
-        run = self.client.beta.threads.runs.create(
-        thread_id=self.thread.id,
-        assistant_id=self.assistant_id,
-        )
-
-        # Wait for the run to complete
-        while run.status != "completed":
-            run = self.client.beta.threads.runs.retrieve(
+        try:
+            message = self.client.beta.threads.messages.create(
             thread_id=self.thread.id,
-            run_id=run.id
+            role="user",
+            content=f"{query}"
             )
-            time.sleep(1)
+            run = self.client.beta.threads.runs.create(
+            thread_id=self.thread.id,
+            assistant_id=self.assistant_id,
+            )
 
-        messages = self.client.beta.threads.messages.list(
-        thread_id=self.thread.id
-        ) 
+            while run.status != "completed":
+                run = self.client.beta.threads.runs.retrieve(
+                thread_id=self.thread.id,
+                run_id=run.id
+                )
+                time.sleep(1)
 
-        return messages
+            messages = self.client.beta.threads.messages.list(
+            thread_id=self.thread.id
+            ) 
+
+            return messages
+        
+        except Exception as e:
+            print(e)
+            return None
+
+    def close_thread(self):
+        """
+        Closes the thread to free up resources.
+        """
+        self.client.beta.threads.delete(thread_id=self.thread.id)
+
+class Proof_sketch_controller(Controller):
+
+    '''A controller for proof sketch GPTs.
+    Each initialization should start an individual thread, which we will append messages to.'''
+    def __init__(self, client, assistant_id):
+        super().__init__(client, assistant_id)
+
+    def add_proof_examples(self, problem_description):
+        """
+        Find and add proof examples to the GPT query.
+        """
+        # Implement logic to add context to the problem description
+        enhanced_query = problem_description  # Placeholder
+        return enhanced_query
 
     def expand_proof(self, response):
         """
@@ -74,5 +87,22 @@ class Proof_sketch_controller:
                 return "Proof validation failed."
         else:
             return "Failed to generate proof sketch."
+
+class Autoformalizer_controller(Controller):
+
+    '''Controller for autoformalizer GPTs. Call for examples and translate from English to formal math.'''
+
+    def __init__(self, client, assistant_id):
+        super().__init__(client, assistant_id)
+
+    def add_translation_examples(self, problem_description):
+        """
+        Adds translation examples to the GPT query.
+        """
+        # Implement logic to add translation examples to the problem description
+        enhanced_query = problem_description  # Placeholder
+        return enhanced_query
+    
+
 
 
